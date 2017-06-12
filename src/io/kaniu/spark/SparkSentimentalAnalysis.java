cfa1021c17e65541244d1ab6a06ab1810088f7cd
kndungu@alumni.usc.edu <kaniu.ndungu@neustar.biz>
2017-06-11 20:58:00 -0700

    Added attribute filter; using JavaESStream to push json string

--- src/io/kaniu/spark/SparkSentimentalAnalysis.java
+++ src/io/kaniu/spark/SparkSentimentalAnalysis.java
@@ -3,32 +3,33 @@ package io.kaniu.spark;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
-import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaPairInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka.KafkaUtils;
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.map.ObjectMapper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import kafka.serializer.DefaultDecoder;
 import kafka.serializer.StringDecoder;
 import scala.Tuple2;
 
-import org.elasticsearch.spark.rdd.EsSpark;
+import org.elasticsearch.spark.streaming.EsSparkStreaming;
+import org.elasticsearch.spark.streaming.api.java.JavaEsSparkStreaming;
 
 import io.kaniu.nlp.NLPWorker;
 
@@ -37,12 +38,15 @@ public class SparkSentimentalAnalysis implements Serializable{
 	
 	static final Logger log = Logger.getLogger(SparkSentimentalAnalysis.class);
 	
-	static private Properties consumerProperties = null;
+	static private Properties consumerProperties = new Properties();
+	static private Properties esProperties = new Properties();
 	static{
-		consumerProperties = new Properties();
+		
 		InputStream producerprops = SparkTweetStreamer.class.getClassLoader().getResourceAsStream("kafkaconsumer.properties");
+		InputStream esprops = SparkTweetStreamer.class.getClassLoader().getResourceAsStream("es-mappings.properties");
 		try {
 			consumerProperties.load(producerprops);
+			esProperties.load(esprops);
 		} catch (IOException e) {
 			log.error(e);
 			// TODO Auto-generated catch block
@@ -70,6 +74,13 @@ public class SparkSentimentalAnalysis implements Serializable{
 		      sparkConf.setMaster("local[2]");
 		 }
 		
+		//set ES properties to spark
+		Set<Entry<Object, Object>> esEntries = esProperties.entrySet();
+		for(Entry<Object, Object> en : esEntries){
+			sparkConf.set(en.getKey().toString(), en.getValue().toString());
+		}
+		
+		
 		// Create the context with 2000 milliseconds batch size
 		JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, new Duration(2000));
 
@@ -90,55 +101,53 @@ public class SparkSentimentalAnalysis implements Serializable{
 				topicsSet
 				);
 
-		messages.foreachRDD(new Function<JavaPairRDD<String, byte[]>, Void>() {
+		JavaDStream<String> lines = messages.map(new Function<Tuple2<String, byte[]>, String>() {
+
 			private static final long serialVersionUID = 1L;
 
 			@Override
-			public Void call(JavaPairRDD<String, byte[]> rdd) throws IOException, ClassNotFoundException, SQLException {
-				
-				rdd.foreachPartitionAsync( new VoidFunction<Iterator<Tuple2<String, byte[]>>>(){
+			public String call(Tuple2<String, byte[]> tuple2) throws Exception {
+				try {
+
+					log.debug("Raw data : Append to hdfs");
+					ObjectMapper mapper = new ObjectMapper();
+					ObjectNode json = (ObjectNode) mapper.readTree(tuple2._2);
 					
+					json = filterAttributes(json);
 					
-					/**
-					 * 
-					 */
-					private static final long serialVersionUID = 1L;
+					//String jsonStr = mapper.readValue(,new TypeReference<String>(){});
+					String text =  json.get("text").textValue();
+					//System.out.println("text ---> "+text);
 
-					@Override
-					public void call(Iterator<Tuple2<String, byte[]>> itTuple) throws Exception {
-						
-						while(itTuple.hasNext()){
-
-							Tuple2<String, byte[]> tuple2  = itTuple.next();					
-
-	
-							try {
-
-								log.debug("Raw data : Append to hdfs");
-								ObjectMapper mapper = new ObjectMapper();
-								JsonNode json = mapper.readTree(tuple2._2);
-								//String jsonStr = mapper.readValue(,new TypeReference<String>(){});
-								String text =  json.get("text").getTextValue();
-								
-
-								String sentText = NLPWorker.sentiment(text);
-								System.out.println(sentText);
-							} catch (Exception e) {
-								//check error and decide if to recycle msg if parser error.
-								
-								log.error(e,e);
-								
-							}
-
-						}
-
-					}
-
-				});	
-
+					int sentiment = NLPWorker.sentiment(text);
+					json.put("sentiment", sentiment);
+					
+					//System.out.println("data ---> "+json.toString());
+					
+					return json.toString();
+					
+				} catch (Exception e) {
+					//check error and decide if to recycle msg if parser error.
+					
+					log.error(e,e);
+					
+				}
 				return null;
 			}
-		});
+
+			private ObjectNode filterAttributes(ObjectNode _json) {
+				ObjectNode json = JsonNodeFactory.instance.objectNode();
+				json.put("text",_json.get("text"));
+				json.put("geoLocation",_json.get("geoLocation"));
+				json.put("createdAt",_json.get("createdAt"));
+				return json;
+			}});
+		
+		//lines.print();
+		JavaEsSparkStreaming.saveJsonToEs(lines, "mine_index/tweet");
+		//EsSparkStreaming.saveJsonToEs(lines.dstream(), "spark/docs");
+		
+		
 
 		jssc.start();
 		jssc.awaitTermination();
